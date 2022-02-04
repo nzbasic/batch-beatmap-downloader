838678521c89d85c6dea6be145a0bc0bac39e4e0
nzbasic <jamescoppard024@gmail.com>
2022-02-04 22:55:17 +1300

    init project structure

---
+++ cmd/api-server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Hello world, the web server
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nzbasic/batch-beatmap-downloader
+
+go 1.13
