f6dd51a0df629cde4f79061cae9f49e5619d51b3
James <54062686+nzbasic@users.noreply.github.com>
2022-06-01 19:53:20 +1200

    use real db metrics

--- api/metrics/database.go
+++ api/metrics/database.go
@@ -1,5 +1,7 @@
 package metrics
 
+import "github.com/nzbasic/batch-beatmap-downloader/database"
+
 type DatabaseMetrics struct {
 	NumberStoredRanked   int
 	NumberStoredLoved    int
@@ -8,19 +10,19 @@ type DatabaseMetrics struct {
 }
 
 func GetDatabaseMetrics() DatabaseMetrics {
-	// var time int
-	// var ranked int
-	// var unranked int
-	// var loved int
-	// database.QueryRow("SELECT approvedDate FROM beatmaps ORDER BY approvedDate DESC LIMIT 1").Scan(&time)
-	// database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'ranked'").Scan(&ranked)
-	// database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'loved'").Scan(&loved)
-	// database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'unranked'").Scan(&unranked)
+	var time int
+	var ranked int
+	var unranked int
+	var loved int
+	database.QueryRow("SELECT approvedDate FROM beatmaps ORDER BY approvedDate DESC LIMIT 1").Scan(&time)
+	database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'ranked'").Scan(&ranked)
+	database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'loved'").Scan(&loved)
+	database.QueryRow("SELECT COUNT(*) FROM beatmaps WHERE Approved = 'unranked'").Scan(&unranked)
 
 	return DatabaseMetrics{
-		NumberStoredRanked:   115167,
-		NumberStoredLoved:    6152,
-		NumberStoredUnranked: 0,
-		LastBeatmapAdded:     1643626800000,
+		NumberStoredRanked:   ranked,
+		NumberStoredLoved:    loved,
+		NumberStoredUnranked: unranked,
+		LastBeatmapAdded:     time,
 	}
 }
