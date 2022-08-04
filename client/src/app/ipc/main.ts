a8fdfb9b00a57284646610293156999e1c93ad41
James <54062686+nzbasic@users.noreply.github.com>
2022-08-04 15:54:34 +1200

    remove imports

--- client/src/app/ipc/main.ts
+++ client/src/app/ipc/main.ts
@@ -1,15 +1,4 @@
-import { app, dialog, ipcMain } from "electron";
-import { Node } from "../../models/filter";
-import axios from "axios";
-import { BeatmapDetails, FilterResponse } from "../../models/api";
-import settings from "electron-settings";
-import { SettingsObject } from "../../global";
-import { download, getDownloadStatus } from "../download";
-import { loadBeatmaps } from "../beatmaps";
-import { window } from '../../main'
-import fs from 'fs'
-import { Metrics } from "../../models/metrics";
-import { checkCollections } from "../collection/collection";
+import { app, ipcMain } from "electron";
 
 export const serverUri = "https://api.nzbasic.com";
 
