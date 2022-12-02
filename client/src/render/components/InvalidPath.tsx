15933ed3d3f49a08ea9a3938d7961dea776adb8c
James <54062686+nzbasic@users.noreply.github.com>
2022-12-03 00:59:43 +1300

    Adjust style on invalid path view

--- client/src/render/components/InvalidPath.tsx
+++ client/src/render/components/InvalidPath.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const InvalidPath = () => {
   return (
-    <div className="bg-white dark:bg-monokai-dark rounded shadow p-6 flex flex-col dark:text-white w-full">
+    <div className="content-box p-6 flex flex-col dark:text-white w-full">
       <span className="font-bold text-lg">Please set a valid osu! path above to use this app.</span>
     </div>
   )
